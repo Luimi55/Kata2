8fcdefbb097529aee7af9828ba4d3eaa65d558fb
Luimi55 <63078542+Luimi55@users.noreply.github.com>
2021-06-04 20:46:16 -0400

    Initial Commit

---
+++ romanNumber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  romanNumber
+//
+//  Created by Luis Miguel Rosa Reyes on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ romanNumber/romanNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  romanNumberApp.swift
+//  romanNumber
+//
+//  Created by Luis Miguel Rosa Reyes on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct romanNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
